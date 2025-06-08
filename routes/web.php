aa83fe0469831c3fd5819a813165d231b6c78ef5
Ahmad Ammar Musyaffa <ahmad.ammar.2205356@students.um.ac.id>
2025-06-08 08:08:22 +0700

    fix(routes): fix all project's routes

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,10 @@
 
 use App\Http\Controllers\CourseController;
 use App\Http\Controllers\CourseQuestionController;
+use App\Http\Controllers\CourseStudentController;
 use App\Http\Controllers\LearningController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\StudentAnswerController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -33,12 +35,42 @@ Route::middleware('auth')->group(function () {
             ->name('course.question.store')
             ->middleware('role:teacher');
 
+        Route::resource('course_questions', CourseQuestionController::class)
+            ->middleware('role:teacher')
+            ->name('learning.index', 'learning');
+
+        Route::get('/course/students/show/{courseId}', [CourseStudentController::class, 'index'])
+            ->name('course.students.show')
+            ->middleware('role:teacher');
+
+        Route::get('/course/students/create/{courseId}', [CourseStudentController::class, 'create'])
+            ->name('course.students.create')
+            ->middleware('role:teacher');
+
+        Route::post('/course/students/store/{courseId}', [CourseStudentController::class, 'store'])
+            ->name('course.students.store')
+            ->middleware('role:teacher');
 
 
-            
         // Student role routes
         Route::get('/learning', [LearningController::class, 'index'])
             ->name('learning.index')
+            ->middleware('role:student');
+
+        Route::get('/learning/finished/{courseId}', [LearningController::class, 'learning_finished'])
+            ->name('learning.finished')
+            ->middleware('role:student');
+
+        Route::get('/learning/rapport/{courseId}', [LearningController::class, 'learning_rapport'])
+            ->name('learning.report')
+            ->middleware('role:student');
+
+        Route::get('/learning/{courseId}/{questionId}', [LearningController::class, 'learning'])
+            ->name('learning.course')
+            ->middleware('role:student');
+
+        Route::post('/learning/answer/{courseId}/{questionId}', [StudentAnswerController::class, 'store'])
+            ->name('learning.answer')
             ->middleware('role:student');
     });
 });
